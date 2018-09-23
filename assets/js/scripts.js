3fbd067e39cd484f67ba6e4410bb00fcae3b4e52
Živilė Stankevičienė <n.zivile@yahoo.com>
2018-09-23 21:30:56 +0300

    Calendar time block created and scroll added.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
     $(".modal--content--left").mCustomScrollbar({theme:"dark"});
+    $(".modal--calendar--times--block").mCustomScrollbar({theme:"minimal-dark"});
 });
